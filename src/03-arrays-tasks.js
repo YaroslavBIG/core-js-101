58604dcc58a39a0ccea10542134ef8bcc2af5551
YaroslavBIG <yaroslavbig@ya.ru>
2020-05-17 16:45:11 +0200

    feet: 82 pass

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -574,8 +574,11 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const centerIdx = Math.floor(arr.length / 2);
+  const tail = arr.splice(-centerIdx);
+  const head = arr.splice(0, centerIdx);
+  return tail.concat(arr).concat(head);
 }
 
 
